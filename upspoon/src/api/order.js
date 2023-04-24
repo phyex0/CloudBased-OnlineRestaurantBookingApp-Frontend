7c1c030637dc5ec35a6e60a039ca80f3f24fca04
Mertcan Kose <mertcankose97@outlook.com>
2023-04-24 17:58:41 +0300

    update env local

--- upspoon/src/api/order.js
+++ upspoon/src/api/order.js
@@ -183,6 +183,10 @@ export const getOrganizationsByBusinessType = async (
     size: 1,
   }
 ) => {
+  console.log("env: ", import.meta.env.VITE_API_DEV);
+  console.log("apiUrl: ", apiUrl);
+  console.log("orderUrl: ", orderUrl);
+
   return await resolve(
     axios
       .get(`${orderUrl}/business/${businessTypes}`, {

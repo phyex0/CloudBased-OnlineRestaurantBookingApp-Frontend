b36e78f4e399f4900fb9699b135d60f640364986
Mertcan Kose <mertcankose97@outlook.com>
2023-05-22 19:40:05 +0300

    add menu api to restaurant

--- upspoon/src/pages/Restaurant/Restaurant.jsx
+++ upspoon/src/pages/Restaurant/Restaurant.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, useContext } from "react";
 import styles from "../../styles/restaurant/RestaurantHome.module.css";
 import {
+  createMenu,
   createProduct,
   getMenu,
   getProduct,
@@ -97,9 +98,19 @@ const Restaurant = () => {
   const [restaurantMenus, setRestaurantMenus] = useState([]);
   const [addProductModalIsOpen, setAddProductModalIsOpen] = useState(false);
   const [activeMenuData, setActiveMenuData] = useState(null);
+  const [addMenuModal, setAddMenuModal] = useState(false);
 
   // product
   const [product, setProduct] = useState({
+    productCode: "",
+    productName: "",
+    description: "",
+    price: 0,
+    productImage: "",
+  });
+
+  const [menu, setMenu] = useState({
+    menuName: "",
     productCode: "",
     productName: "",
     description: "",
@@ -135,6 +146,21 @@ const Restaurant = () => {
       activeMenuData.id,
       product
     );
+
+    console.log("data: ", data);
+    console.log("error: ", error);
+
+    setAddProductModalIsOpen(false);
+  };
+
+  const addMenu = async () => {
+    console.log("organizationId: ", organizationId);
+    console.log("menu: ", menu);
+
+    let { data, error } = await createMenu(organizationId, {
+      name: menu.menuName,
+      productList: [menu],
+    });
 
     console.log("data: ", data);
     console.log("error: ", error);
@@ -230,8 +256,118 @@ const Restaurant = () => {
           <Input placeholder='' size='md' />
         </div> */}
       </Modal>
+      <Modal
+        isOpen={addMenuModal}
+        // onAfterOpen={afterOpenModal}
+        onRequestClose={() => {
+          setAddMenuModal(false);
+        }}
+        className="bg-white relative flex flex-col w-full md:w-1/2 mx-auto mt-20 dark:bg-gray-800 dark:text-white rounded-md shadow-md px-6 py-4"
+        contentLabel="Add Menu"
+      >
+        <button
+          className="absolute right-3 top-3"
+          onClick={() => {
+            setAddProductModalIsOpen(false);
+          }}
+        >
+          <Close width="22" height="22" />
+        </button>
+        <h2>
+          Add Menu to <span className="font-semibold">Restaurant</span>
+        </h2>
+
+        <div className="flex flex-col gap-3 mt-6">
+          <div className="flex flex-col gap-1">
+            <p className={styles.modalTitle}>Menu Name</p>
+            <Input
+              placeholder="Enter Menu Name"
+              size="md"
+              value={menu.menuName}
+              onChange={(e) => {
+                setMenu({ ...menu, menuName: e.target.value });
+              }}
+            />
+          </div>
+
+          <h4 className="font-semibold mt-4 mb-1 text-lg">Menüye Ürün Ekle</h4>
+
+          <div className="flex flex-col gap-1">
+            <p className={styles.modalTitle}>Product Code</p>
+            <Input
+              placeholder="Enter Product Code"
+              size="md"
+              value={menu.productCode}
+              onChange={(e) => {
+                setMenu({ ...menu, productCode: e.target.value });
+              }}
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <p className={styles.modalTitle}>Product Name</p>
+            <Input
+              placeholder="Enter Product Name"
+              size="md"
+              value={menu.productName}
+              onChange={(e) => {
+                setMenu({ ...menu, productName: e.target.value });
+              }}
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <p className={styles.modalTitle}>Product Description</p>
+            <Input
+              placeholder="Enter Product Description"
+              size="md"
+              value={menu.description}
+              onChange={(e) => {
+                setMenu({ ...menu, description: e.target.value });
+              }}
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <p className={styles.modalTitle}>Product Price</p>
+            <Input
+              placeholder="Enter Product Price"
+              size="md"
+              value={menu.price}
+              onChange={(e) => {
+                setMenu({ ...menu, price: e.target.value });
+              }}
+            />
+          </div>
+        </div>
+
+        <button
+          className="border rounded-md px-3 py-2 mr-auto mt-4 font-semibold bg-blue-600 text-white"
+          onClick={() => {
+            addMenu();
+          }}
+        >
+          Add Menu
+        </button>
+
+        {/* <div className="flex flex-col gap-2">
+          <p className={styles.modalTitle}>Product Image</p>
+          <Input placeholder='' size='md' />
+        </div> */}
+      </Modal>
       <div className={styles.menuSection}>
-        <h4 className={styles.title}>My Restaurant Menu</h4>
+        <div className="flex justify-between items-center mb-4">
+          <h4 className={styles.title}>My Restaurant Menu</h4>
+          <button
+            className="border rounded-md px-3 py-2 font-semibold bg-blue-600 text-white"
+            onClick={() => {
+              setAddMenuModal(true);
+            }}
+          >
+            Add Menu
+          </button>
+        </div>
+
         <div className={styles.menusContainer}>
           {restaurantMenus?.map((menu) => (
             <Menu

9ded839ff4554a0eb3646f941657ddeffefadd2e
thesammet <50338990+thesammet@users.noreply.github.com>
2023-05-30 22:45:58 +0300

    refactoring: env local values

--- upspoon/src/components/Oauth2Login/index.jsx
+++ upspoon/src/components/Oauth2Login/index.jsx
@@ -3,17 +3,17 @@ import LoginOAuth2 from "@okteto/react-oauth2-login";
 import axios from "axios";
 import * as querystring from "querystring";
 import AuthContext from "../../context/Auth";
+const url_auth = import.meta.env.VITE_API_AUTH
 
 const Oauth2Login = ({ className, type = "", ...props }) => {
   const onSuccess = (response) => getAccessToken(response.code);
   const onFailure = (response) => console.error(response);
-
   const { setIsAuthUser, setIsAuthRestaurant } = useContext(AuthContext);
 
   const getAccessToken = (responseCode) => {
     axios
       .post(
-        "http://a10b0652697734cc0af5ec1643e74586-1635581591.us-east-1.elb.amazonaws.com/oauth2/token",
+        `${url_auth}/oauth2/token`,
         querystring.stringify({
           grant_type: "authorization_code",
           code: responseCode,
@@ -43,7 +43,7 @@ const Oauth2Login = ({ className, type = "", ...props }) => {
   return (
     <LoginOAuth2
       clientId="upspoon"
-      authorizeUri="http://a10b0652697734cc0af5ec1643e74586-1635581591.us-east-1.elb.amazonaws.com/oauth2/authorize"
+      authorizeUri={`${url_auth}/oauth2/token`}
       redirectUri="http://127.0.0.1:5173"
       scope="openid"
       responseType="code"
